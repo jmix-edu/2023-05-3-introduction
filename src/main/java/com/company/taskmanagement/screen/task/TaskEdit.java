d244d31dcac62921c183576395c6588278a51254
Sergei Iudaev <s.iudaev@haulmont.com>
2023-05-22 12:48:53 +0200

    Select default user on task-edit screen

--- src/main/java/com/company/taskmanagement/screen/task/TaskEdit.java
+++ src/main/java/com/company/taskmanagement/screen/task/TaskEdit.java
@@ -1,10 +1,23 @@
 package com.company.taskmanagement.screen.task;
 
+import com.company.taskmanagement.entity.User;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.ui.screen.*;
 import com.company.taskmanagement.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("tm_Task.edit")
 @UiDescriptor("task-edit.xml")
 @EditedEntityContainer("taskDc")
 public class TaskEdit extends StandardEditor<Task> {
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<Task> event) {
+        User currentUser = (User) currentAuthentication.getUser();
+        event.getEntity().setAssignee(currentUser);
+    }
+
+
 }
